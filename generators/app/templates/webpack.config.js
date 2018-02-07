5f7e6202202281ef3269f1fdc164a0b5dd9ea104
kk.shen <kk.shen@saybot.com>
2018-02-07 09:33:21 +0800

    webpack support

---
+++ generators/app/templates/webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require("webpack");
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: {
+    "index": './src/index.ts',
+    "index.min": './src/index.ts',
+  },
+  output: {
+    filename: 'dist/[name].js'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader' },
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'babel-loader!ts-loader' }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true
+    })
+  ]
+}
